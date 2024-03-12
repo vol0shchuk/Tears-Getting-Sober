cace55b6dbea63a4e2299ae4e1d37747fd0063b4
vol0shchuk <olegvolosuk97@gmail.com>
2024-03-12 17:40:59 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿public class CoffeeMachine
+{
+    public delegate void CoffeeMachineStartedEventHandler();
+    public event CoffeeMachineStartedEventHandler CoffeeMachineStarted;
+
+    public delegate void CoffeeBrewedEventHandler(string coffeeType);
+    public event CoffeeBrewedEventHandler CoffeeBrewed;
+
+    public void StartCoffeeMachine()
+    {
+        Console.WriteLine("Coffee machine is starting...");
+        OnCoffeeMachineStarted();
+    }
+
+    protected virtual void OnCoffeeMachineStarted()
+    {
+        CoffeeMachineStarted?.Invoke();
+    }
+
+    public void BrewCoffee(string coffeeType)
+    {
+        Console.WriteLine($"Brewing {coffeeType} coffee...");
+        OnCoffeeBrewed(coffeeType);
+    }
+
+    protected virtual void OnCoffeeBrewed(string coffeeType)
+    {
+        CoffeeBrewed?.Invoke(coffeeType);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        coffeeMachine.CoffeeMachineStarted += () => Console.WriteLine("Coffee machine is now running.");
+        coffeeMachine.CoffeeBrewed += (coffeeType) => Console.WriteLine($"{coffeeType} coffee is ready.");
+
+        coffeeMachine.StartCoffeeMachine();
+
+        coffeeMachine.BrewCoffee("Late");
+    }
+}
